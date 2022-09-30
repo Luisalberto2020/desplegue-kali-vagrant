ae671b0f48e2afc4a422663054c9e38aa8fe26ef
Luis Alberto Valle <68388768+Luisalberto2020@users.noreply.github.com>
2022-09-30 19:49:49 +0200

    volatiti

--- script.sh
+++ script.sh
@@ -77,6 +77,11 @@ apt install gobuster -y
 
 updatedb #Actualiza el locate
 
+cd /opt
+git clone https://github.com/volatilityfoundation/volatility3.git
+cd volatility3
+python3 setup.py install
+
 userdel -f -r vagrant
 
 shutdown -r now
